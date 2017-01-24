0c07dc313051aad19aaa28c8ea59d1685df34bb4
yuhaifei <1129639939@qq.com>
2017-01-24 14:46:25 +0800

    a

---
+++ EmoticonKey/EmoticonKey/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EmoticonKey
+//
+//  Created by 虞海飞 on 2017/1/24.
+//  Copyright © 2017年 虞海飞. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
