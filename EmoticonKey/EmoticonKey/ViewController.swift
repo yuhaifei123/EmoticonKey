1de9a2efe16639d55688c7eb8735160d40d73f4b
yuhaifei <1129639939@qq.com>
2017-02-06 16:21:36 +0800

    a

--- EmoticonKey/EmoticonKey/ViewController.swift
+++ EmoticonKey/EmoticonKey/ViewController.swift
@@ -27,8 +27,6 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    
-       
     }
 
     
@@ -36,52 +34,7 @@ class ViewController: UIViewController {
         
         
         self!.textView.textViewAttributed(emotion: emotion, font: 17);
-        /*
-        //点击了删除按钮
-        if emotion.removButton {
-            
-            //删除前面的 text
-            if let selft = self{
-                selft.textView.deleteBackward();
-                return;
-            }
-        }
-        
-        //判断是不是emoji 表情
-        if emotion.emojiStr != nil {
-            
-            //  self.textView.replace 替换 text
-            //  self.textView.selectedTextRange 选中的 text
-            self!.textView.replace(self!.textView.selectedTextRange!, withText: emotion.emojiStr!);
-        }
-        
-        if emotion.png  != nil {
-            //创建附件
-            let attachment = NSTextAttachment();
-            attachment.image = UIImage(contentsOfFile: emotion.imagePath!);
-            attachment.bounds = CGRect(x: 0, y: -4, width: 17, height: 17);
-            //根据附件，创建属性字符串
-            let imageText = NSAttributedString(attachment: attachment);
-            
-            //强行解包
-            if let text = self?.textView.text {
-               
-                //拿到当前所有数据 内容
-                let strM = NSMutableAttributedString(attributedString: self!.textView.attributedText);
-                //得到光标地址
-                let rang = self!.textView.selectedRange;
-                strM.replaceCharacters(in: rang, with: imageText);
-                //替换数据
-                self!.textView.attributedText = strM;
-                
-                //附件自己的字体大小
-                strM.addAttribute(NSFontAttributeName, value: UIFont.systemFont(ofSize: 17), range: NSMakeRange(rang.location - 1, 1) );
-                //光标 + 1 ，光标显示数量是0
-                self!.textView.selectedRange = NSMakeRange(rang.location + 1, 0);
-            }
-        }
-        */
-    };
+        };
     
     deinit {
         
